25ad18ee51294795a090f3829034932219ecd2d6
balraj-johal <balraj-johal@users.noreply.github.com>
2019-03-28 16:45:21 +0000

    google maps elaced w leaflet

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
-/// <reference types="@types/googlemaps" />
 import { Component, OnInit, ViewChild } from '@angular/core';
+import * as L from 'leaflet';
+
 
 @Component({
   selector: 'app-map',
@@ -9,19 +10,47 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 
 export class MapComponent implements OnInit {
 
-  @ViewChild('gmap') gmapElement: any;
-  map: google.maps.Map;
+  map;
+  defaultLat;
+  defaultLng;
+  markers: [];
+
   constructor() { }
 
   ngOnInit() {
+    this.defaultLat = 50.5; //where map and user marker are initiated
+    this.defaultLng = 30.5;
+
+    this.map = L.map('map').setView([this.defaultLat, this.defaultLng], 13);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoiZ2FybW9uYm96aWEiLCJhIjoiY2p0c3NoOWp1MHA2eDRnbnBxYm1hOWQwdyJ9.p6481fF0iYHLy5CQFVLMeA'
+  }).addTo(this.map);
+
+    this.newFixedMarker(50.5, 30.5, 'FUCK');
+    this.newDraggableMarker(50.52, 30.52);
   }
 
-  initMap() {
-    alert('MAP')
-    this.map = new google.maps.Map(document.getElementById('map'), {
-      center: new google.maps.LatLng(39.305, -76.617),
-      zoom: 8
-    });
+  newFixedMarker(lat, lng, title) {
+    this.markers.push(
+      L.marker([lat, lng], {
+      title: title,
+      alt: title,
+      riseOnHover: true,
+
+    }).addTo(this.map);
+  })
+
+  newDraggableMarker(lat, lng) {
+    L.marker([lat, lng], {
+      title: 'New Event',
+      alt: 'New Event',
+      riseOnHover: true,
+      draggable: true
+
+    }).addTo(this.map);
   }
 
 }
