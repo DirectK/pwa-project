093502f2960d6c82447c92f2bd725ba21cdb9e6a
balraj-johal <balraj-johal@users.noreply.github.com>
2019-03-29 13:30:19 +0000

    map for adding event works!

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import * as L from 'leaflet';
 
 
@@ -10,18 +10,20 @@ import * as L from 'leaflet';
 
 export class MapComponent implements OnInit {
 
-  map;
-  defaultLat;
-  defaultLng;
-  markers: [];
+  map: L.map;
+  lat;
+  lng;
+  location;
+  fixedMarkers: L.marker[] = [];
+  draggableMarker = L.marker;
 
   constructor() { }
 
   ngOnInit() {
-    this.defaultLat = 50.5; //where map and user marker are initiated
-    this.defaultLng = 30.5;
+    this.lat = 50.5; //where map and user marker are initiated
+    this.lng = 30.5;
 
-    this.map = L.map('map').setView([this.defaultLat, this.defaultLng], 13);
+    this.map = L.map('map').setView([this.lat, this.lng], 13);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
@@ -31,26 +33,35 @@ export class MapComponent implements OnInit {
 
     this.newFixedMarker(50.5, 30.5, 'FUCK');
     this.newDraggableMarker(50.52, 30.52);
+    this.draggableMarker.on('move', this.getDragMarkerLocation());
   }
 
   newFixedMarker(lat, lng, title) {
-    this.markers.push(
-      L.marker([lat, lng], {
+    var marker = L.marker([lat, lng], {
       title: title,
       alt: title,
       riseOnHover: true,
-
     }).addTo(this.map);
-  })
+
+    this.fixedMarkers.push(marker);
+  }
 
   newDraggableMarker(lat, lng) {
-    L.marker([lat, lng], {
+    var marker = L.marker([lat, lng], {
       title: 'New Event',
       alt: 'New Event',
       riseOnHover: true,
       draggable: true
-
     }).addTo(this.map);
+
+    this.draggableMarker = marker;
+
+  }
+
+  getDragMarkerLocation() {
+    this.location = this.draggableMarker.getLatLng();
+    this.lat = this.location.lat;
+    this.lng = this.location.lng;
   }
 
 }
