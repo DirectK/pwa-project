d5094a499bc03d8f30ba0f5c0319b0ab95801634
Andrei Clopotel <andreivalentin44@yahoo.com>
2019-03-30 15:58:37 +0000

    Added dummy image to events

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -1,8 +1,6 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Event } from './event';
 import { IdbService } from './idb.service';
-
-import { unwrap, IDBPDatabase } from 'idb'
 
 @Injectable({
   providedIn: 'root'
