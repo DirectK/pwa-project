39f8753fb1526b35ca8bbd4d567495c61ce8a294
balraj-johal <balraj-johal@users.noreply.github.com>
2019-05-19 17:51:19 +0100

    urgent fix allowing credentials in http req

--- dist/server.js
+++ dist/server.js
@@ -13,7 +13,6 @@ const fs = require('fs')
 
 const port = 3000
 const corsOptions = {
-  origin: 'http://localhost:4200'
 }
 
 const filesToIgnore = [
@@ -36,12 +35,14 @@ app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Origin", "localhost:4200");
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
      next();
 });
 app.use(cors({
+  origin: 'http://localhost:4200',
   credentials: true,
 }));
 
